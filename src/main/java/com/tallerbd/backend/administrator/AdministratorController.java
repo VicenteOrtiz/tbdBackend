8c160b4423be36acc55543f1e49ae554d5fd7de1
vicente <vicente.hernandez@usach.cl>
2019-09-21 01:55:56 -0300

    UPDATE: admin crud without delete

--- src/main/java/com/tallerbd/backend/administrator/AdministratorController.java
+++ src/main/java/com/tallerbd/backend/administrator/AdministratorController.java
@@ -7,7 +7,9 @@ import com.tallerbd.backend.user.UserRepository;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/admin")
 public class AdministratorController{
 
+    /*
+    This controller only works with user and role entity.
+    There isn't an admin entity because an admin user doesn't have agregated atributes (yet),
+    as oposed to coordinator or volunteer entities.
+    */
+
+    private final String roleName = "admin";
+
     private final UserRepository userRepository;
 
 	private final RoleRepository roleRepository;
@@ -28,28 +38,45 @@ public class AdministratorController{
 		this.roleRepository = roleRepository;
     }
 
-    @GetMapping()
-    public ResponseEntity getAllAdmin(){
-        Role admin = roleRepository.findByName("admin");
-        return new ResponseEntity<>( admin.getUsers() , HttpStatus.OK);
-
-    }
-
     private void checkOrCreateAdminRole(){
-        if( roleRepository.findByName("admin") == null ){
+        if( roleRepository.findByName(this.roleName) == null ){
             Role admin = new Role();
-            admin.setName("admin");
+            admin.setName(this.roleName);
             roleRepository.save(admin);
         }
     }
+
+    @GetMapping()
+    public ResponseEntity getAllAdminUsers(){
+        checkOrCreateAdminRole();
+        Role admin = roleRepository.findByName(this.roleName);
+        return new ResponseEntity<>( admin.getUsers() , HttpStatus.OK);
+    }
+
+    @GetMapping("/{user_id}")
+    public ResponseEntity getAdminUserById(@PathVariable Long user_id){
+        User target = userRepository.findById(user_id).get();
+        if( target == null ){
+            return new ResponseEntity<>("User not found", HttpStatus.BAD_REQUEST);
+        }else{
+            checkOrCreateAdminRole();
+            Role admin = roleRepository.findByName(this.roleName);
+
+            if( target.getRole().getId() == admin.getId() ){
+                return new ResponseEntity<>(target, HttpStatus.OK);
+            }else{
+                return new ResponseEntity<>("User is not administrator", HttpStatus.BAD_REQUEST);
+            }
+        }
+    }
     
-    @GetMapping("/users/{id}")
+    @GetMapping("/user/{id}")
     public ResponseEntity setAdminRoleToUser(@PathVariable Long id){
         User target = userRepository.findById(id).get();
 
         checkOrCreateAdminRole();
 
-        Role admin = roleRepository.findByName("admin");
+        Role admin = roleRepository.findByName(this.roleName);
         target.setRole(admin);
         
         return new ResponseEntity<>(userRepository.save(target), HttpStatus.OK);
@@ -58,13 +85,15 @@ public class AdministratorController{
     @PostMapping()
     public ResponseEntity createAdmin(@RequestBody User user){
         if( userRepository.findByEmail( user.getEmail() ) == null ){
+
             checkOrCreateAdminRole();
-            Role admin = roleRepository.findByName("admin");
+            Role admin = roleRepository.findByName(this.roleName);
+
             user.setRole(admin);
+
             return new ResponseEntity<>(userRepository.save(user), HttpStatus.CREATED);
         }else{
             return new ResponseEntity<>("An User with that email already exist", HttpStatus.BAD_REQUEST);
         }
-    }
-    
+    }    
 }
